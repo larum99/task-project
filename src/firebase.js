0f6886cd9ad18ebb8aca6c3e8959cb1ec1903797
Laura <laura.barona@uao.edu.co>
2025-02-12 21:27:08 -0500

    project config

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js"
-import { } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-firestore.js"
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,3 +16,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
