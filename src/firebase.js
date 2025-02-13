6b297c937d55f410677e578c5aaa42a2311cfccf
Laura <laura.barona@uao.edu.co>
2025-02-12 21:41:58 -0500

    added components and .env

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyB93cezc7tcxqJlrV37dijXVSuJqgQ9bBM",
-    authDomain: "task-project-f38cd.firebaseapp.com",
-    projectId: "task-project-f38cd",
-    storageBucket: "task-project-f38cd.firebasestorage.app",
-    messagingSenderId: "257554058405",
-    appId: "1:257554058405:web:477133ca6dee8f8479514c"
-};
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+}
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
 
-export { db };
+export { db }
