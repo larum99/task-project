bca3b4065fc49e7e014530a78466c8bc80c8cfab
Laura <laura.barona@uao.edu.co>
2025-02-12 20:51:41 -0500

    firebase config file

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-app.js"
+import { } from "https://www.gstatic.com/firebasejs/11.3.0/firebase-firestore.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyB93cezc7tcxqJlrV37dijXVSuJqgQ9bBM",
+    authDomain: "task-project-f38cd.firebaseapp.com",
+    projectId: "task-project-f38cd",
+    storageBucket: "task-project-f38cd.firebasestorage.app",
+    messagingSenderId: "257554058405",
+    appId: "1:257554058405:web:477133ca6dee8f8479514c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
